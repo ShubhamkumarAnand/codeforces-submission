878c03b99a0df45f78b7f54397c4c476296a842c
imskanand <the.anand3099@gmail.com>
2022-07-29 23:47:52 +0530

    Become Formless

---
+++ Special_Number_Finding_from_a_2_-D_Matrix.cpp
@@ -0,0 +1,41 @@
+/*
+   * Author :  imskanand
+   ^ Time   : 29 July 2022 (23:30)
+*/
+
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using i64 = long long;
+
+#ifdef DEBUG
+#include "algo/debug.h"
+#else
+#define debug(...)
+#endif
+
+#define all(x) x.begin(), x.end()
+#define sort(x) sort(all(x));
+#define desc(x) sort(all(x), greater<int>{});
+#define dbe(x) cout << x << endl;
+#define dbz(x) cout << x << " ";
+
+void solve() {
+  // ^ https://www.codechef.com/IEMC2022/problems/IEMCO10_P2V1
+  // TODO
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  // cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
