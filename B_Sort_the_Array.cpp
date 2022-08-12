3d2297601fc3fb5191c1ef6c18ead269f3d182d9
imskanand <the.anand3099@gmail.com>
2022-08-12 20:53:43 +0530

    Rakshabandhan Special

--- B_Sort_the_Array.cpp
+++ B_Sort_the_Array.cpp
@@ -11,58 +11,29 @@
 using namespace std;
 using i64 = long long;
 
-#ifdef DEBUG
-#include "algo/debug.h"
-#else
-#define debug(...)
-#endif
-
-#define ll long long
-#define v(x) vector<int> v(x)
-#define all(x) x.begin(), x.end()
-#define sort(x) sort(all(x));
-#define desc(x) sort(all(x), greater<int>{});
-#define dbe(x) cout << x << endl;
-#define dbz(x) cout << x << " ";
-
-void solve() {
-  int n;
-  cin >> n;
-  v(n);
-  for (int i = 0; i < n; i++) {
-    cin >> v[i];
-  }
-  vector<int> s(n);
-  s = v;
-  sort(s);
-  // TODO
-  int count = 0;
-  vector<int> a;
-  debug(v);
-  debug(s);
-  for (int i = 0; i < n; i++) {
-    if (count > 2) {
-      dbe("no");
-      return;
-    }
-    if (s[i] != v[i]) {
-      count++;
-      a.push_back(s[i]);
-    }
-  }
-  debug(count);
-  dbe("yes");
-  cout << a[0] << " " << a[1] << endl;
-}
-
+int n, k, i, a[100001], b[100001], p1, p2;
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-
-  int t = 1;
-  // cin >> t;
-  while (t--) {
-    solve();
+  cin >> n;
+  for (i = 0; i < n; i++) {
+    cin >> a[i];
+    b[i] = a[i];
   }
-  return 0;
+
+  sort(b, b + n);
+  for (i = 0; i < n; i++) {
+    if (b[i] != a[i]) {
+      p2 = i;
+      k++;
+      if (k == 1)
+        p1 = i;
+    }
+  }
+  reverse(a + p1, a + 1 + p2);
+  for (i = 0; i < n; i++) {
+    if (b[i] != a[i]) {
+      cout << "no";
+      return 0;
+    }
+  }
+  cout << "yes" << endl << p1 + 1 << " " << p2 + 1;
 }
