11b5624474758d82615b1d17a6dd2d5de45d0e36
imskanand <the.anand3099@gmail.com>
2022-08-22 23:23:36 +0530

    Head Pain!!

--- B_Interesting_Sum.cpp
+++ B_Interesting_Sum.cpp
@@ -18,21 +18,48 @@ using i64 = long long;
 #endif
 
 #define ll long long
-#define v(x) vector<int> v(x)
+// #define a(x) vector<int> v(x)
 #define all(x) x.begin(), x.end()
 #define sort(x) sort(all(x));
 #define desc(x) sort(all(x), greater<int>{});
 #define dbe(x) cout << x << endl;
 #define dbz(x) cout << x << " ";
 
+constexpr int inf = std::numeric_limits<int>::max();
+
 void solve() {
   int n;
   cin >> n;
-  v(n);
+  vector<int> a(n);
   for (int i = 0; i < n; i++) {
-    cin >> v[i];
+    cin >> a[i];
   }
   // TODO
+  vector dp(3, vector<int>(16, -inf));
+  dp[0][0] = dp[1][0] = 0;
+  for (int i = 0; i < n; i++) {
+    vector g(3, vector<int>(16, -inf));
+    for (int j = 0; j <= 2; j++) {
+      for (int s = 0; s < 16; s++) {
+        if (dp[j][s] == -inf) {
+          continue;
+        }
+        for (int t = 0; t < 4; t++) {
+          int f = t << (j % 2 * 2);
+          if (s & f) {
+            continue;
+          }
+          int ns = s | f;
+          int val = (t & 1) * a[i] - (t >> 1 & 1) * a[i];
+          g[j][ns] = max(g[j][ns], dp[j][s] + val);
+          if (j < 2) {
+            g[j + 1][ns] = max(g[j + 1][ns], dp[j][s] + val);
+          }
+        }
+      }
+    }
+    dp = g;
+  }
 }
 
 int main() {
