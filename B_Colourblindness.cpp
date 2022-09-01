43050698a8c70411239d59d2a497fa77bab3ded0
imskanand <the.anand3099@gmail.com>
2022-09-01 23:05:34 +0530

    B Accepted!!

---
+++ B_Colourblindness.cpp
@@ -0,0 +1,55 @@
+/*
+   * Author :  imskanand
+   ^ Time   : 01 September 2022 (23:00)
+*/
+
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using i64 = long long;
+
+#ifdef DEBUG
+#include "algo/debug.h"
+#else
+#define debug(...)
+#endif
+
+#define ll long long
+#define v(x) vector<int> v(x)
+#define all(x) x.begin(), x.end()
+#define sort(x) sort(all(x));
+#define desc(x) sort(all(x), greater<int>{});
+#define dbe(x) cout << x << endl;
+#define dbz(x) cout << x << " ";
+
+void solve() {
+  int n;
+  cin >> n;
+  string a, b;
+  cin >> a >> b;
+  for (int i = 0; i < n; i++) {
+    if ((a[i] == 'G' && b[i] == 'B') or (a[i] == 'B' && b[i] == 'G') or
+        (a[i] == b[i])) {
+      continue;
+    } else {
+      dbe("NO");
+      return;
+    }
+  }
+  dbe("YES");
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
