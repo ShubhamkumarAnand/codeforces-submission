717317261657bc0f53295d1493c5ffe60fa5190a
imskanand <the.anand3099@gmail.com>
2022-09-04 06:05:37 +0530

    Time to Get A JOB!!

---
+++ A_Crossmarket.cpp
@@ -0,0 +1,44 @@
+/*
+   * Author :  imskanand
+   ^ Time   : 04 September 2022 (06:03)
+*/
+
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using i64 = long long;
+
+#ifdef DEBUG
+#include "algo/debug.h"
+#else
+#define debug(...)
+#endif
+
+#define ll long long
+#define v(x) vector<int> v(x)
+#define all(x) x.begin(), x.end()
+#define sort(x) sort(all(x));
+#define desc(x) sort(all(x), greater<int>{});
+#define dbe(x) cout << x << endl;
+#define dbz(x) cout << x << " ";
+
+void solve() {
+  int n, m;
+  cin >> n >> m;
+  dbe((n - 1 + m - 1 + min(min(1, n - 1) + m - 1, min(1, m - 1) + n - 1)));
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
